bf71cb8956a71162219122a7eece70fa389ffb9b
Amethyst Software <contact@amethystsoftware.com>
2022-01-26 16:00:52 -0500

    scrape_smugmug_images.sh: Download GIFs as well as JPEGs.

--- Bash/scrape_smugmug_images.sh
+++ Bash/scrape_smugmug_images.sh
@@ -4,12 +4,12 @@
 # Parameter 1: URL of a Smugmug gallery
 # Parameter 2: Output directory for images
 # Recommended width:
-# |---------------------------------------------------------------------------------------------------------------------|
+# |-----------------------------------------------------------------------------------------------------------------------|
 
 GALLERY_PAGE="$1"
 OUTPUT_DIR="$2"
 
-AGENT="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36 OPR/58.0.3135.79"
+AGENT="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36 OPR/82.0.4227.43"
 
 # Safety check
 if [ ! "$#" -eq 2 ]; then
@@ -31,21 +31,23 @@ RSS_FEED="${DOMAIN}hack/feed.mg?Type=gallery&Data=$RSS_ID&ImageCount=9999&Paging
 echo "Saving images using gallery's RSS feed at $RSS_FEED..."
 
 # Load gallery page and find thumbnail URLs, then guess full-size image URLs from those
-for IMAGE in `curl --silent --insecure --user-agent '"$AGENT"' "$RSS_FEED" | grep -o "src=\"https:.*\.jpg"`; do
-   THUMB_IMG=${IMAGE#src=\"}
+for SUFFIX in jpg gif; do
+   for IMAGE in `curl --silent --insecure --user-agent '"$AGENT"' "$RSS_FEED" | grep -o "src=\"https:.*\.$SUFFIX"`; do
+      THUMB_IMG=${IMAGE#src=\"}
 
-   # Turn thumbnail URL into largest possible image by replacing 'Th's with 'O's
-   ORIG_IMG=$(echo "$THUMB_IMG" | sed 's/-Th\.jpg$/-O.jpg/')
-   ORIG_IMG=$(echo "$ORIG_IMG" | sed 's@/Th/@/O/@')
+      # Turn thumbnail URL into largest possible image by replacing 'Th's with 'O's
+      ORIG_IMG=$(echo "$THUMB_IMG" | sed "s/-Th\.${SUFFIX}$/-O.${SUFFIX}/")
+      ORIG_IMG=$(echo "$ORIG_IMG" | sed 's@/Th/@/O/@')
 
-   JUST_ID=${ORIG_IMG##*/}
-   echo "Downloading $ORIG_IMG..."
+      JUST_ID=${ORIG_IMG##*/}
+      echo "Downloading $ORIG_IMG..."
 
-   # Call 'curl' with "-L" to follow redirects because Smugmug often redirects a call for an "O" image to something
-   # like an "X3" image at a different URL
-   curl -o "$OUTPUT_DIR/$JUST_ID" -L --silent --insecure --user-agent '"$AGENT"' "$ORIG_IMG"
-   CURL_ERR=$(echo $?)
-   if [ $CURL_ERR != 0 ]; then
-      echo "Download failed with code $CURL_ERR."
-   fi
+      # Call 'curl' with "-L" to follow redirects because Smugmug often redirects a call for an "O" image to something
+      # like an "X3" image at a different URL
+      curl -o "$OUTPUT_DIR/$JUST_ID" -L --silent --insecure --user-agent '"$AGENT"' "$ORIG_IMG"
+      CURL_ERR=$(echo $?)
+      if [ $CURL_ERR != 0 ]; then
+         echo "Download failed with code $CURL_ERR."
+      fi
+   done
 done
