1d015999b8fbc4ce043d240cf4d295241b877284
Amethyst Software <contact@amethystsoftware.com>
2022-09-09 11:23:51 -0400

    run_script_on_files_by_size.sh: Updated script name internally. Now printing size of each file.

--- Bash/run_script_on_files_by_size.sh
+++ Bash/run_script_on_files_by_size.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-# Run On Files By Size
+# Run Script on Files by Size
 # Looking at the files in a given directory, pass each one to the specified script if it
 # is within a certain size range. Parameters:
 # 1. Script to run files on.
 # 2. Directory to look in for files.
 # 3. Minimum size in bytes of files to select.
 # 4. Maximum size in bytes of files to select.
+# Recommended width:
+# |---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ----|
 
 # Get all files below specified size in given directory, then place them in a sortable
 # array
@@ -34,6 +36,6 @@ for THE_FILE in ${SORTED_FILE_DATA[@]}; do
    IFS="|"
    declare -a DATA_PARTS=($THE_FILE)
    FILE_NAME=${DATA_PARTS[1]}
-   echo -e "\n---Running script on file $FILE_NAME...---"
+   echo -e "\n---Running script on file $FILE_NAME (${DATA_PARTS[0]} bytes)...---"
    bash "$1" "$2/$FILE_NAME"
 done
