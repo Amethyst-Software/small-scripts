45575e93b03bb23c2ec028e11918a72b87b02f7a
Amethyst Software <contact@amethystsoftware.com>
2022-09-08 22:32:21 -0400

    print_cert_info.sh: No longer recursively searching target directory. Results are obtained far faster and in alphabetical order. Made output more readable.

--- Bash/print_cert_info.sh
+++ Bash/print_cert_info.sh
@@ -26,8 +26,9 @@ TEMP_INFO=~/Downloads/cert_info.txt
 
 # Find all items that have ".app" at the end of their path but not
 # ".app/" in the midst of their path (i.e., not .apps within .apps)
-for next_item in `find "$1" | grep "\.app$" | egrep -v "\.app/"`; do
-  echo "$next_item:"
+for THE_APP in `ls "$1" | grep "\.app$"`; do
+#for THE_APP in `find "$1" | grep "\.app$" | egrep -v "\.app/"`; do
+  echo "$(tput bold)$THE_APP:$(tput sgr0)"
 
   # Save stdout and stderr to file descriptors 3 and 4, then redirect
   # them to a file. If we could send this to a variable, it would be
@@ -37,7 +38,7 @@ for next_item in `find "$1" | grep "\.app$" | egrep -v "\.app/"`; do
   exec 3>&1 4>&2 > "$TEMP_INFO" 2>&1
 
   # Get certificate info with enough detail to show the Authority lines
-  codesign --display --verbose=2 "$next_item"
+  codesign --display --verbose=2 "$1/$THE_APP"
 
   # Restore stdout and stderr
   exec 1>&3 2>&4
