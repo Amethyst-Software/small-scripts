30dd92d570be08042216513fd20f1a87a45befa6
Amethyst Software <contact@amethystsoftware.com>
2023-02-05 12:15:46 -0500

    Rewrote duplicate_file_and_copy_names.sh to take a text file as the source for new file names, and to accept a custom suffix for the created files.

--- Bash/duplicate_file_and_copy_names.sh
+++ Bash/duplicate_file_and_copy_names.sh
@@ -1,16 +1,82 @@
 #!/bin/bash
 
 # Duplicate File and Copy Names
-# Duplicates a single file and names each copy with the same name as the
-# files found in a given folder. In other words, the contents of the file
-# passed as $1 will be copied into folder $3 once for every file in folder
-# $2, with each copy named after one of those files. The suffix of the
-# duplicated files is also changed to the file extension hardcoded below.
+# Duplicates a single file and names each copy after one of the files
+# found in a given folder or one of the lines found in a text file.
+# An optional suffix can be applied to the duplicated files in place
+# of the original.
 
-COPYFILE="$1"
-COPYNAMES="$2"
-DESTFILES="$3"
+IFS="
+"
 
-for each_file in `ls "$COPYNAMES"`; do
-   cp "$COPYFILE" "$DESTFILES"/${each_file%.*}.new-suffix
+COPY_FILE=""
+COPY_NAMES=""
+DEST_DIR=""
+NEW_SUFFIX=""
+FROM_FILE=-1
+
+# Process arguments
+while (( "$#" >= 2 )); do
+  case "$1" in
+      --copy-file )  COPY_FILE="$2"; shift 2;;
+      --copy-names ) COPY_NAMES="$2"; shift 2;;
+      --dest )       DEST_DIR="$2"; shift 2;;
+      --new-suffix ) NEW_SUFFIX="$2"; shift 2;;
+      * )            echo "Unrecognized argument $1."; exit;;
+   esac
 done
+
+if [ -z "$COPY_FILE" ] || [ -z "$COPY_NAMES" ] || [ -z "$DEST_DIR" ]; then
+   echo "You need to specify three arguments:"
+   echo "  '--copy-file': a file to copy"
+   echo "  '--copy-names': the source for the names that will be applied to the copies (can be either a text file with a list of names or a directory of files from which to copy the names)"
+   echo "  '--dest': a destination direction"
+   echo "Optional argument:"
+   echo "  '--new-suffix': the suffix to use for the copies, in place of the original file's suffix"
+   exit
+fi
+
+if [ ! -f "$COPY_FILE" ]; then
+   echo "The file '$COPY_FILE' doesn't exist!"
+   exit
+fi
+
+if [ ! -f "$COPY_NAMES" ]; then
+   if [ ! -d "$COPY_NAMES" ]; then
+      echo "There is no file or directory at '$COPY_NAMES'!"
+      exit
+   else
+      FROM_FILE=0
+   fi
+else
+   FROM_FILE=1
+fi
+
+answer="n"
+if [ ! -d "$DEST_DIR" ]; then
+   echo "The specified destination directory '$DEST_DIR' doesn't exist. Create it? (y/n)"
+   read answer
+   if [ "$answer" == "y" ]; then
+      mkdir -p "$DEST_DIR"
+   else
+      echo "Exiting."
+      exit
+   fi
+fi
+
+# Copy and name files
+if [ $FROM_FILE -eq 0 ]; then
+   for THE_FILE in `ls "$COPY_NAMES"`; do
+      if [ -z "$NEW_SUFFIX" ]; then
+         cp "$COPY_FILE" "$DEST_DIR/$THE_FILE"
+      else
+         cp "$COPY_FILE" "$DEST_DIR/${THE_FILE%.*}.$NEW_SUFFIX"
+      fi
+   done
+elif [ $FROM_FILE -eq 1 ]; then
+   for THE_LINE in `cat "$NAME_LIST"`; do
+      if [ ! -f "$DEST_DIR/$THE_LINE" ]; then
+         cp "$COPY_FILE" "$DEST_DIR/$THE_LINE"
+      fi
+   done
+fi
