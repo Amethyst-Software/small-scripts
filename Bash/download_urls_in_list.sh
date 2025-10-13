ff84816b9e9824cd263eb0ea82eac0869e594d09
Amethyst Software <contact@amethystsoftware.com>
2025-10-12 23:19:02 -0400

    download_urls_in_list.sh: Added optional sleep argument to avoid throttling.

--- Bash/download_urls_in_list.sh
+++ Bash/download_urls_in_list.sh
@@ -4,6 +4,13 @@
 # This script calls upon 'curl' to download a list of URLs.
 # Parameter 1: The path to a text file full of URLs pointing to files.
 # Parameter 2: The target directory for the downloads.
+# Parameter 3 (optional): The amount of seconds to sleep between downloads
+# (to avoid rate limits on some sites).
+
+SLEEP=0
+if [ ! -z "$3" ]; then
+   SLEEP=$3
+fi
 
 IFS="
 "
@@ -21,5 +28,8 @@ fi
 for URL in `cat "$1"`; do
    FILE_NAME=${URL##*/}
    echo "Downloading $FILE_NAME..."
+   if [ $SLEEP > 0 ]; then
+      sleep $SLEEP
+   fi
    curl -L -o "$2/$FILE_NAME" --max-time 10 "$URL"
 done
